fb1015719452f699d347ccdb6fb464c9f3fcbcbe
Florença de Melo Junqueira <jflor.melo@gmail.com>
2023-03-08 23:31:00 -0300

    Subindo parte principal do projeto

--- mvp/src/App.js
+++ mvp/src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import Formulario from './Componentes/Formulario';
 
 function App() {
+
+  let colaboradores = []
+
+  const aoNovoColaboradorCadastrato = (colaborador) =>{
+    colaboradores.push(colaborador)
+    localStorage.setItem("colaboradores", JSON.stringify(colaboradores))
+  }
+
   return (
     <div className="App">
-      <Formulario/>
+      <Formulario aoColaboradorCadastrato={(colaborador => aoNovoColaboradorCadastrato(colaborador))} />
     </div>
   );
 }
--- mvp/src/Componentes/Formulario/index.js
+++ mvp/src/Componentes/Formulario/index.js
@@ -5,7 +5,7 @@ import "./Formulario.css"
 import { useState } from "react"
 
 
-const Formulario = () => {
+const Formulario = (props) => {
 
   const [nome, setNome] = useState("")
   const [idade, setIdade] = useState("")
@@ -14,16 +14,22 @@ const Formulario = () => {
 
   const AoSalvar = (evento) => {
     evento.preventDefault()
-    console.log
   }
+
+  props.aoColaboradorCadastrado({
+    nome : nome,
+    idade : idade,
+    email : email,
+    alternativa : alternativa
+  })
 
   return(
     <section>
       <form onSubmit={AoSalvar}>
-        <CampoTexto texto="Digite seu nome"/>
-        <CampoTexto texto="Digite sua idade"/>
+        <CampoTexto texto="Digite seu nome" AoAlterado={valor => setNome(valor)}/>
+        <CampoTexto texto="Digite sua idade" AoAlterado={valor => setIdade(valor)}/>
         <CampoTexto texto="Digite seu email" AoAlterado={valor => setEmail(valor)}/> 
-        <Check texto="Selecione uma das alternativas"/>
+        <Check texto="Selecione uma das alternativas" AoAlterado={valor => setAlternativa(valor)}/>
         <Botao/>
       </form>
     </section>
